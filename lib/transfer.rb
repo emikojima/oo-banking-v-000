1db6bca683ea6fafcb6fd621f183543a86b75827
emi kojima <emi8emi@gmail.com>
2018-03-08 05:29:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
  
  def reverse_transfer
    #binding.pry
-   if @self.execute_transaction == true
+   if @self.execute_transaction == "complete"
    @receiver.balance -= amount 
    @sender.deposit(amount)
    @status = "reversed"
